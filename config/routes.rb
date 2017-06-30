6defcbd711d46adb8080a43edaad9bbb725aa3c9
amin <shadecute3@gmail.com>
2017-06-30 05:00:03 +0430

    Add Contact page

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 # defualt routes
   root 'pages#home'
   get  'about'=>'pages#about'
+  get  'contact'=> 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
