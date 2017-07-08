f898960558295b8a195800773ae63b2c896ac849
amin <shadecute3@gmail.com>
2017-07-09 00:43:57 +0430

    Added Post resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
 
-   has_many :tweets     
+   has_many :tweets  
+   has_many :posts  
 
 end
